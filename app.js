70d9a562d2d869a784e409ba1414c9507538bf7f
narut-o <akash_ranjan__@outlook.com>
2022-09-20 16:39:40 +0530

    new branch

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const userSchema  = new mongoose.Schema({
 });
 
 
-userSchema.plugin(encrypt,{secret:process.env.SECRET,encryptedFields:["password"]});
+userSchema.plugin(encrypt,{secret:process.env.SECRET,encryptedFields:["password"]}); 
 
 const User = mongoose.model("User",userSchema);
 
